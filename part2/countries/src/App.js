c6f47426987c3022ea7505e7a6d5fc61d17fec3f
David Chavez <depch47@gmail.com>
2022-01-05 12:02:06 -0500

    getting the countries using axios and logging them for now.

--- part2/countries/src/App.js
+++ part2/countries/src/App.js
@@ -1,25 +1,32 @@
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
+import { useEffect, useState } from 'react'
+import axios from 'axios'
+const App = () => {
+  const [countries, setCountries] = useState([])
+  const [search, setSearch] = useState('')
+  useEffect(() => {
+    const getCountries = async () => {
+      const res = await axios.get('https://restcountries.com/v3.1/all')
+      // before setting the data, add a filter to check if the country name contains "search"
+      const filteredList = res.data.filter((c) => c.name.common.includes(search))
+      console.log(filteredList)
+      setCountries(filteredList)
+    }
+    getCountries()
+  }, [search])
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <>
+      <div>
+        <label htmlFor=''>
+          find countries <input value={search} onChange={(e) => setSearch(e.target.value)} />
+        </label>
+      </div>
+      <div>
+        {countries.length > 10 && <p>Too many matches, specify another filter</p>}
+        {countries.length <= 10 && countries.length > 1 && <p>Render list of 10 countries' names</p>}
+        {countries.length === 1 && <p>Show info of one country</p>}
+      </div>
+    </>
+  )
 }
 
-export default App;
+export default App
