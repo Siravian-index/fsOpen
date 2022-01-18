0524463c635292a751d5c741f2ea0505fe3e930a
David Chavez <depch47@gmail.com>
2022-01-18 11:02:16 -0500

    install supertest which helps with the testing of http operations. Also added a test file for GET /api/blogs. Still playing around with jest making tests

--- part4/blog-list/src/utils/config/mongDB.js
+++ part4/blog-list/src/utils/config/mongDB.js
@@ -2,7 +2,10 @@ const mongoose = require('mongoose')
 const logger = require('../log/logger')
 
 const PORT = process.env.PORT
-const MONGODB_URI = process.env.MONGODB_URI
+// const MONGODB_URI = process.env.MONGODB_URI
+// It now checks the env before assigning a URI
+const MONGODB_URI = process.env.NODE_ENV === 'test' ? process.env.TEST_MONGODB_URI : process.env.MONGODB_URI
+
 const connectDb = async () => {
   try {
     await mongoose.connect(MONGODB_URI)
