4ca938c721b560ffdc15f032f7607d1c85115282
David Chavez <depch47@gmail.com>
2022-01-18 14:47:34 -0500

    added some 'mock' blogs to test

--- part4/blog-list/src/test/blogs_api.test.js
+++ part4/blog-list/src/test/blogs_api.test.js
@@ -1,8 +1,20 @@
 const mongoose = require('mongoose')
 const supertest = require('supertest')
 const app = require('../app')
-
 const api = supertest(app)
+const { Blog } = require('../models/blogSchema')
+const initialBlogs = [
+  { title: 'testing', author: 'david', url: 'www.reddit.com/myPost' },
+  { title: 'Sample', author: 'Jhon', url: 'www.reddit.com/hisPost' },
+]
+
+beforeEach(async () => {
+  await Blog.deleteMany({})
+  let blogObj = new Blog(initialBlogs[0])
+  await blogObj.save()
+  blogObj = new Blog(initialBlogs[1])
+  await blogObj.save()
+})
 
 test('blogs are returned as json', async () => {
   await api
