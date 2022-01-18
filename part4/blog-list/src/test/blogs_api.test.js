dba9ad0055419678ccf954296774237a9f7bf952
David Chavez <depch47@gmail.com>
2022-01-18 16:50:19 -0500

    added two test to check getting one blog and deleting one blog. Now I need to create those routes.

--- part4/blog-list/src/test/blogs_api.test.js
+++ part4/blog-list/src/test/blogs_api.test.js
@@ -48,6 +48,27 @@ describe('tests blog routes', () => {
     const blogsAtEnd = await blogsInDB()
     expect(blogsAtEnd).toHaveLength(initialBlogs.length)
   })
+
+  test('a specific blog can be viewed', async () => {
+    const blogsAtStart = await blogsInDB()
+    const blogsToView = blogsAtStart[0]
+    const resultBlog = await api
+      .get(`/api/blogs/${blogsToView.id}`)
+      .expect(200)
+      .expect('Content-Type', /application\/json/)
+    const processedBlogToView = JSON.parse(JSON.stringify(blogsToView))
+    expect(resultBlog.body).toEqual(processedBlogToView)
+  })
+
+  test('a blog can be deleted', async () => {
+    const blogsAtStart = await blogsInDB()
+    const blogToDelete = blogsAtStart[0]
+    await api.delete(`/api/blogs/${blogToDelete.id}`).expect(204)
+    const blogsAtEnd = await blogsInDB()
+    expect(blogsAtEnd).toHaveLength(initialBlogs.length - 1)
+    const contents = blogsAtEnd.map((b) => b.title)
+    expect(contents).not.toContain(blogToDelete.title)
+  })
 })
 
 afterAll(() => {
