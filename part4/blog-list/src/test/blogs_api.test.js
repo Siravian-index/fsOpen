da1526203a582f9a1a8a6c6d05e78133d5dcf610
David Chavez <depch47@gmail.com>
2022-01-19 20:57:14 -0500

    clean up

--- part4/blog-list/src/test/blogs_api.test.js
+++ part4/blog-list/src/test/blogs_api.test.js
@@ -8,83 +8,11 @@ const { initialBlogs, nonExistingId, blogsInDB } = require('./test_helper')
 // fills the DB with two obj before the test run
 beforeEach(async () => {
   await Blog.deleteMany({})
-  // Not so DRY option
-  // let blogObj = new Blog(initialBlogs[0])
-  // await blogObj.save()
-  // blogObj = new Blog(initialBlogs[1])
-  // await blogObj.save()
-  // ----------------
-  // Good ol' for, nothing beats it.
   for (let blog of initialBlogs) {
     let blogObj = new Blog(blog)
     await blogObj.save()
   }
-  // ----------------
-  // advanced solution using Promise.all()
-  // const blogObjects = initialBlogs.map((blog) => new Blog(blog))
-  // const promiseArray = blogObjects.map((blog) => blog.save())
-  // console.log(promiseArray)
-  // await Promise.all(promiseArray)
-  // ----------------
 })
-
-// describe('basic test on the DB', () => {
-//   test('blogs are returned as json', async () => {
-//     await api
-//       .get('/api/blogs')
-//       .expect(200)
-//       .expect('Content-Type', /application\/json/)
-//   })
-
-//   test('there is a total of two blogs', async () => {
-//     const response = await api.get('/api/blogs')
-//     expect(response.body).toHaveLength(initialBlogs.length)
-//   })
-// })
-
-// describe('tests blog routes', () => {
-//   test('a valid blog can be added', async () => {
-// const newBlog = { title: 'test with jest', author: 'jester', url: 'jest.com' }
-// await api
-//   .post('/api/blogs')
-//   .send(newBlog)
-//   .expect(201)
-//   .expect('Content-Type', /application\/json/i)
-
-// const blogsAtEnd = await blogsInDB()
-// expect(blogsAtEnd).toHaveLength(initialBlogs.length + 1)
-// const contents = blogsAtEnd.map((b) => b.title)
-// expect(contents).toContain('test with jest')
-//   })
-
-//   test('blog without title is not added', async () => {
-//     const newBlog = { author: 'jester', url: 'jest.com' }
-//     await api.post('/api/blogs').send(newBlog).expect(400)
-//     const blogsAtEnd = await blogsInDB()
-//     expect(blogsAtEnd).toHaveLength(initialBlogs.length)
-//   })
-
-//   test('a specific blog can be viewed', async () => {
-//     const blogsAtStart = await blogsInDB()
-//     const blogsToView = blogsAtStart[0]
-//     const resultBlog = await api
-//       .get(`/api/blogs/${blogsToView.id}`)
-//       .expect(200)
-//       .expect('Content-Type', /application\/json/)
-//     const processedBlogToView = JSON.parse(JSON.stringify(blogsToView))
-//     expect(resultBlog.body).toEqual(processedBlogToView)
-//   })
-
-// test('a blog can be deleted', async () => {
-//   const blogsAtStart = await blogsInDB()
-//   const blogToDelete = blogsAtStart[0]
-//   await api.delete(`/api/blogs/${blogToDelete.id}`).expect(204)
-//   const blogsAtEnd = await blogsInDB()
-//   expect(blogsAtEnd).toHaveLength(initialBlogs.length - 1)
-//   const contents = blogsAtEnd.map((b) => b.title)
-//   expect(contents).not.toContain(blogToDelete.title)
-// })
-// })
 
 describe('Exercises 4.8-4.12', () => {
   test('4.8: Verify that the blog list application returns the correct amount of blog posts in the JSON format.', async () => {
