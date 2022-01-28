f4df1deb3e6ba95c3aaa53c2418e4767ceab44fc
David Chavez <depch47@gmail.com>
2022-01-28 11:02:05 -0500

    Testing has been a complete fail.

--- part5/bloglist-frontend/src/components/CreateBlog.test.js
+++ part5/bloglist-frontend/src/components/CreateBlog.test.js
@@ -4,13 +4,13 @@ import '@testing-library/jest-dom/extend-expect'
 import CreateBlog from './CreateBlog'
 
 test.only('<BlogForm /> updates parent state and calls onSubmit', () => {
-  const addBlog = jest.fn()
+  const setBlogs = jest.fn()
   const user = {
     token:
       'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VybmFtZSI6ImRhdmluY2hpIiwiaWQiOiI2MWVjODA0NGQxNGNkYmViZWUxODMzYWMiLCJpYXQiOjE2NDMzMzUzODEsImV4cCI6MTY0MzMzODk4MX0.AOVqVyIhdcFTNdH6BqxOOJhDeTlv-DtVYvCnSlfgKmI',
   }
 
-  const component = render(<CreateBlog createBlog={addBlog} user={user} />)
+  const component = render(<CreateBlog setBlogs={setBlogs} user={user} />)
 
   const openForm = component.getByText('create a new blog')
   fireEvent.click(openForm)
@@ -30,8 +30,8 @@ test.only('<BlogForm /> updates parent state and calls onSubmit', () => {
   })
   fireEvent.submit(form)
 
-  expect(addBlog.mock.calls).toHaveLength(1)
-  expect(addBlog.mock.calls[0][0].title).toBe('Blog Title')
-  expect(addBlog.mock.calls[0][0].author).toBe('Author')
-  expect(addBlog.mock.calls[0][0].url).toBe('http://blog-title.com')
+  expect(setBlogs.mock.calls).toHaveLength(1)
+  expect(setBlogs.mock.calls[0][0].title).toBe('Blog Title')
+  expect(setBlogs.mock.calls[0][0].author).toBe('Author')
+  expect(setBlogs.mock.calls[0][0].url).toBe('http://blog-title.com')
 })
